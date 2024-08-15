922b22065db4a67b010a69e52da6c98073c78aee
grubd14 <83056016+grubd14@users.noreply.github.com>
2024-08-15 17:33:29 +0545

    I don't know what I am doing

--- ds-lab/boolean-matrix-operation.c
+++ ds-lab/boolean-matrix-operation.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 #define N 3  // Size of the matrix (N x N)
--- lab-4/banking-systems.cpp
+++ lab-4/banking-systems.cpp
@@ -8,38 +8,58 @@ using namespace std;
 
 class Account {
 public:
-  string account_num = "BAF23554671";
-  double account_amount = 21000.43;
+    string account_num = "BAF23554671";
+    double account_amount = 21000.43;
 
-  virtual void deposit() {
-    cout << "Money is being deposited!";
-  };
+    virtual void deposit(double amount) {
+        account_amount += amount;
+        cout << "Deposited: $" << amount << " | New Balance: $" << account_amount << endl;
+    }
 
-  virtual void withdraw() {
-    cout << "Money is being withdrawn!";
-  }
-
+    virtual void withdraw(double amount) {
+        if (amount > account_amount) {
+            cout << "Insufficient funds. Withdrawal denied." << endl;
+        } else {
+            account_amount -= amount;
+            cout << "Withdrew: $" << amount << " | New Balance: $" << account_amount << endl;
+        }
+    }
 };
 
 class SavingsAccount : public Account {
+private:
+    double interestRate = 5.0; // Interest rate in percentage
+
 public:
-  void deposit() override {
-    cout << "Overidden function of deposit to savings class";
-  }
+    void deposit(double amount) override {
+        Account::deposit(amount); // Use the base class method to add the deposit
+        calculateInterest();
+    }
+
+    void calculateInterest() {
+        double interest = account_amount * (interestRate / 100);
+        account_amount += interest;
+        cout << "Interest added: $" << interest << " | New Balance: $" << account_amount << endl;
+    }
 };
 
 class CheckingAccount : public Account {
 public:
-  void withdraw() override {
-    cout << "Overriden function of withdrawn from checking class";
-  }
-
+    void withdraw(double amount) override {
+        cout << "Overridden function of withdrawal from checking class" << endl;
+        Account::withdraw(amount); // Use the base class method for withdrawal
+    }
 };
 
-
 int main() {
-  CheckingAccount ca1;
-  ca1.withdraw();
+    SavingsAccount sa1;
+    CheckingAccount ca1;
 
+    cout << "Initial Savings Account Balance: $" << sa1.account_amount << endl;
+    sa1.deposit(500.0);
+
+    cout << "\nInitial Checking Account Balance: $" << ca1.account_amount << endl;
+    ca1.withdraw(1000.0);
+
+    return 0;
 }
-
